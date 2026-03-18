cb752c678596061bc4ce9a1a479d837bd3817e97
shotsan <santosh@tamu.edu>
2026-03-18 12:25:07 -0700

    fix: correct artifact filenames in release workflows, cargo-bundle URL, and PDB name

--- script/bundle-windows.ps1
+++ script/bundle-windows.ps1
@@ -170,7 +170,7 @@ function BuildRemoteServer {
 
 function ZipZedAndItsFriendsDebug {
     $items = @(
-        ".\$CargoOutDir\zed-custom.pdb",
+        ".\$CargoOutDir\zed_custom.pdb",
         ".\$CargoOutDir\cli.pdb",
         ".\$CargoOutDir\auto_update_helper.pdb",
         ".\$CargoOutDir\explorer_command_injector.pdb",
